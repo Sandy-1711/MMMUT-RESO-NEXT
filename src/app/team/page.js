2dd26b7bbea1910abc16d344d5e671662580952f
Sandy-1711 <sandy1711003@gmail.com>
2024-01-06 14:56:42 +0530

    team page done

--- src/app/team/page.js
+++ src/app/team/page.js
@@ -1,39 +1,58 @@
 import teamcss from './team.module.css'
+import { data } from './utils/team'
 export default function TeamPage() {
+
     return (<div className={teamcss.main}>
-        <h1>Our Mentors</h1>
-        <div className={teamcss.cards}>
-            <div className={teamcss.card}>
-                <div className={teamcss.profile}>
-                    <img />
-                </div>
-                <p>Prof. J.P. Saini</p>
-                <span>Professor</span>
+        <div className={teamcss.teamWrap} id='wrapper'>
+
+            <h1>Our Mentors</h1>
+            <div className={teamcss.cards}>
+                {data.mentors.map(function (item) {
+                    return <div className={teamcss.card}>
+                        <div className={teamcss.profile}>
+                            <img src={item.src} />
+                        </div>
+                        <p>{item.name}</p>
+                        <span>{item.position}</span>
+                    </div>
+                })}
             </div>
-            <div className={teamcss.card}>
-                <div className={teamcss.profile}>
-                    <img />
-                </div>
-                <p>Prof. S.P. Singh</p>
-                <span>Professor</span>
+            <h1>Our Hardworking Developers</h1>
+            <div className={teamcss.cards}>
+                {data.dev.map(function (item) {
+                    return <div className={teamcss.card}>
+                        <div className={teamcss.profile}>
+                            <img src={item.src} />
+                        </div>
+                        <p>{item.name}</p>
+                        <span>{item.branch}</span>
+                        <div className={teamcss.socials}>
+                            <a href={item.linkedin}></a>
+                            <a href={item.instagram}></a>
+                            <a href={item.mail}></a>
+                            <a href={item.whatsapp}></a>
+                        </div>
+                    </div>
+                })}
+
             </div>
-            <div className={teamcss.card}>
-                <div className={teamcss.profile}>
-                    <img />
-                </div>
-                <p>Dr. D.S. Singh</p>
-                <span>Professor</span>
-            </div>
-        </div>
-        <h1>Our Hardworking Developers</h1>
-        <div className={teamcss.cards}>
-            <div className={teamcss.card}>
-                <div className={teamcss.profile}>
-                    <img />
-                </div>
-                <p>Name</p>
-                <span>Branch</span>
-                <div className={teamcss.socials}></div>
+            <h1>Content Providers</h1>
+            <div className={teamcss.cards}>
+                {data.content.map(function (item) {
+                    return <div className={teamcss.card}>
+                        <div className={teamcss.profile}>
+                            <img src={item.src} />
+                        </div>
+                        <p>{item.name}</p>
+                        <span>{item.branch}</span>
+                        <div className={teamcss.socials}>
+                            <a href={item.linkedin}></a>
+                            <a href={item.instagram}></a>
+                            <a href={item.mail}></a>
+                            <a href={item.whatsapp}></a>
+                        </div>
+                    </div>
+                })}
             </div>
         </div>
     </div>)
